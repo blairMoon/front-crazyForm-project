767c0e1d27de10f5f287bececbae232f848c302a
오수빈 <catherine621@naver.com>
2023-03-03 10:53:25 +0900

    signup_form만들기

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -3,7 +3,27 @@ import React from 'react';
 import css from './Signup.module.scss';
 
 function Signup() {
-  return <div className={css.container}>signup</div>;
+  return (
+    <div className={css.container}>
+      <div className={css.signUpTitle}>
+        <span>회원가입 </span>
+      </div>
+      <div className={css.formDiv}>
+        <form className={css.signFormTag}>
+          <label> 아이디</label>
+          <input className={css.useId} type="text" />
+          <label> 비밀번호</label>
+          <input type="password" />
+          <label> 이름</label>
+          <input className={css.name} type="text" />
+          <label> 이메일</label>
+          <input type="text" />
+          <label> 생년월일</label>
+          <input type="text" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
