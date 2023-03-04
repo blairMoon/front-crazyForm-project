0eb651ce65cdbc068f9774e55b62b66c486b073a
sam101321 <sam010321@gmail.com>
2023-03-05 00:47:44 +0900

    fix/signup8

--- src/pages/Signup/Signup1.js
+++ src/pages/Signup/Signup1.js
@@ -26,7 +26,7 @@ function Signup1() {
         <div className={css.Wrapper}>
           <div className={css.TopBox}>
             <h1>SignUp</h1>
-            <form className={css.Form}>
+            <form onSubmit={handleSubmit(onSubmit)} className={css.Form}>
               <div>
                 <label>아이디</label>
                 <input
@@ -34,7 +34,17 @@ function Signup1() {
                   placeholder="아이디"
                   name="username"
                   type="text"
+                  {...register('username', {
+                    required: true,
+                    maxLength: 20,
+                  })}
                 />
+                {errors.username && errors.username.type === 'required' && (
+                  <p>아이디를 입력하세요!</p>
+                )}
+                {errors.username && errors.username.type === 'maxLength' && (
+                  <p>6자 이하로 입력하세요.</p>
+                )}
               </div>
               <div>
                 <label>비밀번호</label>
@@ -43,25 +53,59 @@ function Signup1() {
                   placeholder="비밀번호"
                   name="password"
                   type="password"
+                  {...register('password', {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                  })}
                 />
+                {errors.password && errors.password.type === 'required' && (
+                  <p>비밀번호를 입력하세요!</p>
+                )}
+                {errors.password && errors.password.type === 'minLength' && (
+                  <p>6자 이상 20자 이하로 입력하세요.</p>
+                )}
+                {errors.password && errors.password.type === 'maxLength' && (
+                  <p>6자 이상 20자 이하로 입력하세요.</p>
+                )}
               </div>
               <div>
                 <label>이름</label>
                 <input
                   className={css.IdPassword}
                   placeholder="이름"
-                  name="username"
+                  name="name"
                   type="text"
+                  {...register('name', {
+                    required: true,
+                    maxLength: 6,
+                  })}
                 />
+                {errors.name && errors.name.type === 'required' && (
+                  <p>이름을 입력하세요!</p>
+                )}
+                {errors.name && errors.name.type === 'maxLength' && (
+                  <p>6자 이하로 입력하세요.</p>
+                )}
               </div>
               <div>
                 <label>닉네임</label>
                 <input
                   className={css.IdPassword}
                   placeholder="닉네임"
-                  name="username"
+                  name="nickname"
                   type="text"
+                  {...register('nickname', {
+                    required: true,
+                    maxLength: 10,
+                  })}
                 />
+                {errors.nickname && errors.nickname.type === 'required' && (
+                  <p>닉네임을 입력하세요!</p>
+                )}
+                {errors.nickname && errors.nickname.type === 'maxLength' && (
+                  <p>10자 이하로 입력하세요.</p>
+                )}
               </div>
               <div>
                 <div>이메일</div>
@@ -70,6 +114,10 @@ function Signup1() {
                   placeholder="이메일"
                   name="email"
                   type="text"
+                  {...register('email', {
+                    required: true,
+                    maxLength: 15,
+                  })}
                 />
                 @
                 <select name="email_select" className={css.Email2}>
@@ -77,6 +125,12 @@ function Signup1() {
                   <option value="naver.com"> naver.com </option>
                   <option value="write"> 직접 입력 </option>
                 </select>
+                {errors.email && errors.email.type === 'required' && (
+                  <p>이메일을 입력하세요!</p>
+                )}
+                {errors.email && errors.email.type === 'maxLength' && (
+                  <p>15자 이하로 입력하세요.</p>
+                )}
               </div>
               <div>
                 <label>생년월일</label>
@@ -85,7 +139,17 @@ function Signup1() {
                   placeholder="생년월일"
                   name="birthday"
                   type="text"
+                  {...register('birthday', {
+                    required: true,
+                    maxLength: 15,
+                  })}
                 />
+                {errors.birthday && errors.birthday.type === 'required' && (
+                  <p>생일을 입력하세요!</p>
+                )}
+                {errors.birthday && errors.birthday.type === 'maxLength' && (
+                  <p>15자 이하로 입력하세요.</p>
+                )}
               </div>
               <button type="submit" value="회원가입" className={css.Button}>
                 회원가입
