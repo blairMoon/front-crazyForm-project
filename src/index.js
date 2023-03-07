6f5eeeeac8dd276cf097df02f5ba981391076985
오수빈 <catherine621@naver.com>
2023-03-07 11:31:16 +0900

    index.js react-query 연결

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom/client';
 import './styles/reset.scss';
 import './styles/common.scss';
 import Router from './pages/Router';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+const queryClient = new QueryClient();
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
