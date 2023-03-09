b9aa78dc7b958e55dff4351d9598f99bc21ef0c0
오수빈 <catherine621@naver.com>
2023-03-09 13:47:43 +0900

    아이디중복체크 구현 editMember

--- src/pages/EditMember/EditMember.js
+++ src/pages/EditMember/EditMember.js
@@ -47,7 +47,10 @@ const EditMember = () => {
   password.current = watch('password');
 
   const checkUsename = id => {
-    return instance.get(`users/@${id}`).then(res => res.data);
+    return instance
+      .get(`users/@${id}`)
+      .then(res => res.data)
+      .then(res => alert(res));
   };
   // console.log('data', data);
   if (data) {
@@ -75,7 +78,7 @@ const EditMember = () => {
 
                 <button
                   className={css.button}
-                  onClick={checkUsename(watch('username'))}
+                  onClick={() => checkUsename(watch('username'))}
                 >
                   아이디 중복확인
                 </button>
