11a0265bc1fb79a45fdcb67dfe37b1ba88823fd5
ramram <tnfkachzh@naver.com>
2023-03-10 17:34:21 +0900

    layout -1

--- src/pages/WholeLectures/WholeLectures.js
+++ src/pages/WholeLectures/WholeLectures.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+
 import css from './WholeLectures.module.scss';
+import { Box, Grid, GridItem, Input, Select } from '@chakra-ui/react';
 
 function WholeLectures() {
   return (
     <>
       <Header />
-
+      <Box p="5">
+        <Box mb="5">
+          <Input placeholder="검색" size="md" />
+          <Select placeholder="카테고리 선택" size="md" ml="2">
+            <option value="frontend">Frontend</option>
+            <option value="backend">Backend</option>
+            <option value="devops">DevOps</option>
+          </Select>
+        </Box>
+        <Grid templateColumns={['repeat(1, 1fr)', 'repeat(3, 1fr)']} gap="5">
+          {Array.from(Array(9), (_, i) => (
+            <GridItem key={i}>
+              <CourseCard />
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
       <Footer />
     </>
   );
