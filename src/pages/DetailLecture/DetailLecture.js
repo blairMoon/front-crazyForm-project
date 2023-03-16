63a6c2b77b03dafdfea01c923bcb09b79c75e4fc
오수빈 <catherine621@naver.com>
2023-03-16 14:40:09 +0900

    pagenation 구현+catagory작업중

--- src/pages/DetailLecture/DetailLecture.js
+++ src/pages/DetailLecture/DetailLecture.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { QueryClient, useQuery, useQueryClient } from '@tanstack/react-query';
-import { getLectureInfo } from '../../api';
+import { getLectureDetail } from '../../api';
 const DetailLecture = () => {
-  const { isLoading, data } = useQuery(['lectureInfo'], getLectureInfo);
   let { id } = useParams();
+  const { isLoading, data } = useQuery(['lectureInfo'], () =>
+    getLectureDetail(id)
+  );
   if (data) {
     {
-      console.log(data.data);
+      console.log('data:', data);
     }
-    return (
-      <div>
-        <div>{id}번째페이지 입니당당당당다아</div>
-        <h1>{data.data[id].lectureTitle}</h1>
-        <h2>{data.data[id].lectureDescription}</h2>
-      </div>
-    );
+    if (data) {
+      return (
+        <div>
+          <div>{id}번째페이지 입니당당당당다아</div>
+          <h1>{data.lectureTitle}</h1>
+          <h2>{data.lectureDescription}</h2>
+          {/* <h2>{data.data[id].lectureDescription}</h2> */}
+        </div>
+      );
+    }
   }
 };
 export default DetailLecture;
