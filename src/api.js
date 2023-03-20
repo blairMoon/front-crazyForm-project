2930c64d13f38e8efade06cfa76206b66d851681
sam101321 <sam010321@gmail.com>
2023-03-20 16:31:08 +0900

    lecture7

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ export const instance = axios.create({
   baseURL: 'http://127.0.0.1:8000/api/v1/',
   headers: {
     'X-CSRFToken': Cookies.get('csrftoken'),
-    Jwt: Cookies.get('token'),
+    Jwt: localStorage.getItem('token'),
   },
   withCredentials: true,
 });
@@ -23,7 +23,7 @@ export async function userNameLogin({ username, password }) {
   if (response.ok) {
     const data = await response.json();
 
-    Cookies.set('token', data.token, { expires: 7, httpOnly: true });
+    window.localStorage.setItem('token', data.token);
     return true;
   } else {
     const { message } = await response.json();
